ed09591eed6b0cb62db381b4eb46bf00e146459f
teegoood <teegooodgood@gmail.com>
2024-06-24 20:41:38 +0700

    add: color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        project: {
+          fuchsia: '#F75471',
+          pink: '#FFBBD2',
+          apricot: '#E9A49B',
+          cream: '#F1DFC1',
+          blue: '#678CEC',
+          'dark-gray': '#979797',
+          'light-gray': '#313131',
+          'light-blue': '#D8E3FE',
+        },
+      },
     },
   },
   plugins: [],
