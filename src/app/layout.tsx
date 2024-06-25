d7e86ec129679845289905dbb82ba28b94f00d3f
teegoood <teegooodgood@gmail.com>
2024-06-25 23:00:44 +0700

    add: gradient background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + 'w-full min-h-screen'}>
+      <body className={inter.className + 'w-full min-h-screen bg-1'}>
         {children}
       </body>
     </html>
