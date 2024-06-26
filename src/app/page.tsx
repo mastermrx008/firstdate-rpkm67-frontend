5ac8ba2429d85ad7c7d7f9998e5b64d50335f27f
teegoood <teegooodgood@gmail.com>
2024-06-26 22:26:07 +0700

    fix: border opacity to bg-opacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Border from '@/components/Border';
 export default function Home() {
   return (
     <main className="w-full h-screen flex justify-center items-center flex-col">
-      <Border variant="dark-pink">
-        <h1 className="text-4xl font-bold text-center text-white">
-          Hello, World!
+      <Border variant="light-pink">
+        <h1 className="text-4xl font-semibold text-center text-white">
+          username
         </h1>
       </Border>
     </main>
