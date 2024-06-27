cb6bfaa23405328dfb2e535c0a5f4ec9f1851aae
Soravit Satechaichan <dunk.soravit@gmail.com>
2024-06-27 22:36:11 +0700

    coming soon SVG

--- src/app/comingsoon/page.tsx
+++ src/app/comingsoon/page.tsx
@@ -1,29 +1,33 @@
+import Image from 'next/image';
 import Border from '@/components/Border';
 import Back_To_Menu from '@/components/comingsoon/back_to_menu';
-import Welcome from '@/components/comingsoon/welcome';
+import Welcome from '@/components/welcome';
+import comingsoon from '../../../public/comingsoon.svg';
 
 export default function Home() {
   return (
     <main className="w-full h-screen flex justify-center items-center flex-col">
       <Border
         variant="transparent"
-        className="relative"
-      />{' '}
-      <Welcome />
-      <div className="absolute">
-        <h1 className="text-2xl text-center font-semibold text-project-light-gray mb-6">
+        className="flex justify-center items-center flex-col"
+      >
+        {' '}
+        <Welcome />
+        <h1 className="text-2xl text-center font-semibold text-project-light-gray mb-7">
           รับเพื่อนก้าวใหม่ 2024
         </h1>
-        <h1 className="text-4xl text-center text-project-light-gray mb-8 font-season italic font-bold">
-          &#39;Coming Soon&#39;
-        </h1>
+        <Image
+          src={comingsoon}
+          alt="Coming Soon"
+          className="w-64 h-14 mb-6"
+        />
         <h1 className="text-xl text-center font-semibold text-project-light-gray mb-10">
           พร้อมให้ลงทะเบียน
           <br />
           ในวันที่ 18 กรกฎาคม 2567
         </h1>
         <Back_To_Menu />
-      </div>
+      </Border>
     </main>
   );
 }
