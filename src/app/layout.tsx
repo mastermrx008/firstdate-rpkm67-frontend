a95c3a3876aab93573472a4f4fffb707206022d7
teegoood <teegooodgood@gmail.com>
2024-06-27 18:43:59 +0700

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from 'next';
 import { Athiti } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 import Footer from '@/components/Footer';
-
-const athiti = Athiti({
-  subsets: ['latin', 'thai'],
-  weight: ['200', '300', '400', '500', '600', '700'],
-});
 
 export const metadata: Metadata = {
   title: 'RPKM',
@@ -19,10 +15,65 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={athiti.className + ' w-full min-h-screen bg-1'}>
+      <body
+        className={`${athiti.variable} ${season.variable} ${sarun.variable} w-full min-h-screen bg-1 font-athiti`}
+      >
         {children}
         <Footer />
       </body>
     </html>
   );
 }
+
+const athiti = Athiti({
+  subsets: ['latin', 'thai'],
+  weight: ['200', '300', '400', '500', '600', '700'],
+  variable: '--athiti',
+});
+
+const season = localFont({
+  variable: '--season',
+  src: [
+    {
+      path: '../../public/fonts/english/theseasons-reg.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/english/theseasons-lt.otf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/english/theseasons-bd.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/english/theseasons-it.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/english/theseasons-ltit.otf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/english/theseasons-bdit.otf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+});
+
+const sarun = localFont({
+  variable: '--sarun',
+  src: [
+    {
+      path: '../../public/fonts/thai/Sarun.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
