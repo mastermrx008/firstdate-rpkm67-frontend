f5630e8ebcc41cb4914fae2effe0f0090c492d2b
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-06-29 17:27:21 +0700

    fix: minor details

--- src/components/emergency-contact/EmergencyCallBlock.tsx
+++ src/components/emergency-contact/EmergencyCallBlock.tsx
@@ -11,14 +11,16 @@ const EmergencyCallBlock: React.FC<EmergencyCallProps> = ({
   phoneNumber,
 }) => {
   return (
-    <div className="shadow-[0_2px_4px_0px_rgba(0,0,0,0.25)] rounded-xl w-60 h-16 flex items-center flex-row justify-between px-3 py-1">
+    <div className="shadow-[0_2px_4px_0px_rgba(0,0,0,0.25)] rounded-xl flex items-center flex-row justify-between px-3 py-1">
       <div className="flex flex-col gap-y-1">
         <p className="font-athiti text-base	font-medium text-black">{text}</p>
-        <p className="text-black text-base opacity-50">{phoneNumber}</p>
+        <p className="font-athiti text-base	font-medium text-black opacity-50">
+          {phoneNumber}
+        </p>
       </div>
-      <div className="flex items-center">
+      <div className="flex justify-center items-center">
         <a href={`tel:${phoneNumber}`}>
-          <button className="shadow-[0_0_4px_0px_rgba(0,0,0,0.25)] flex rounded-full flex-cols w-11 h-11 bg-project-fuchsia  justify-center items-center">
+          <button className="shadow-[0_0_4px_0px_rgba(0,0,0,0.25)] flex rounded-full flex-cols size-11 bg-project-fuchsia justify-center items-center">
             <Icon
               icon="fluent:call-connecting-20-filled"
               className="text-white"
