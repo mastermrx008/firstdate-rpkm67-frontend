c2cdf8cbbddfa27459332f2c28f9d6525a23de08
Soravit Satechaichan <dunk.soravit@gmail.com>
2024-06-29 15:26:14 +0700

    cn utils, change absolute

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { cn } from './lib/utils';
 
-export default function Welcome() {
+interface WelcomeProps {
+  containerClassName?: string; // Optional string prop
+  welcomeClassName?: string;
+  cuClassName?: string;
+}
+
+export default function Welcome({
+  containerClassName,
+  welcomeClassName,
+  cuClassName,
+}: WelcomeProps) {
   return (
-    <div className="absolute top-20 text-center font-season italic">
-      <h1 className="text-4xl mb-1">Welcome,</h1>
-      <h1 className="text-3xl bg-gradient-to-t bg-clip-text text-transparent from-project-fuchsia from-30% via-80% to-95% to-project-cream">
+    <div
+      className={cn(
+        'text-center font-season italic mt-8 mb-28',
+        containerClassName
+      )}
+    >
+      <h1 className={cn('text-4xl mb-1', welcomeClassName)}>Welcome,</h1>
+      <h1
+        className={cn(
+          'text-3xl bg-gradient-to-t bg-clip-text text-transparent from-project-fuchsia from-30% via-80% to-95% to-project-cream',
+          cuClassName
+        )}
+      >
         CU108
       </h1>
     </div>
