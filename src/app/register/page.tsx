248595cd3850f5f7763a08fbb446bb1b53b191f5
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-07-01 23:38:43 +0700

    feat: init register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,16 @@
+import Border from '@/components/Border';
+
+export default function Register() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center flex-col">
+      <Border variant="dark-pink">
+        <h1 className="text-5xl font text-center text-white font-season italic">
+          Welcome,
+        </h1>
+        <h2 className="text-4xl font text-center text-white font-season italic">
+          CU108
+        </h2>
+      </Border>
+    </main>
+  );
+}
