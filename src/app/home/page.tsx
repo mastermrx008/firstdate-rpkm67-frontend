2ab7be22c07e02352b91b5bb1b90d016ac6543b2
jaroonro <charoonroj.amo@gmail.com>
2024-07-02 16:50:25 +0700

    style: adjust position/size

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,18 +12,18 @@ export default function Home() {
     
   return (
     <main className="w-full h-screen flex justify-center items-center flex-col bg-2">
-      <Border variant='transparent'>
-        <div className="max-w-80 w-full h-screen opacity-100">
+      <Border variant='transparent' className="px-[0%] py-[0%]">
+        <div className="flex flex-col w-[90%] h-[90vh] opacity-100 justify-center">
             
             
-            <h1 className="text-center text-black -mt-12 font-theseasons italic" style={{fontSize: '40px', fontFamily: 'var(--font-theseasons)'}}>
+            <h1 className="text-center text-black font-season italic text-[4.4vh]">
             Welcome,
             </h1>
             <div className="flex justify-center items-center">
-                <h1 className="text-center bg-gradient-to-t bg-clip-text text-transparent from-project-fuchsia from-30% via-80% to-95% to-project-cream italic pr-1" style={{fontSize: '26px', fontFamily: 'var(--font-theseasons)'}}>
+                <h1 className="text-center bg-gradient-to-t bg-clip-text text-transparent from-project-fuchsia from-30% via-80% to-95% to-project-cream italic pr-1 font-season text-[2.85vh]">
                 CU
                 </h1>
-                <h1 className="text-center bg-gradient-to-t bg-clip-text text-transparent from-project-fuchsia from-30% via-80% to-95% to-project-cream italic pr-1" style={{fontSize: '28px', fontFamily: 'var(--font-theseasons)'}}>
+                <h1 className="text-center bg-gradient-to-t bg-clip-text text-transparent from-project-fuchsia from-30% via-80% to-95% to-project-cream italic pr-1 font-season text-[3.3vh]">
                 108
                 </h1>
             </div>
@@ -31,17 +31,16 @@ export default function Home() {
 
 
             <center>
-                <div className="flex flex-col items-center text-center gap-y-3 w-36 h-64 self-center">
-                    <div className="w-full h-48 bg-black rounded-t-full border-white shadow-[0px_0px_4px_.4px_#00000036]" style={{borderWidth: '6px', borderStyle: 'solid'}}>
+                <div className="flex flex-col items-center text-center justify-between w-[15.81vh] h-[29.2vh] self-center">
+                    <div className="w-full h-[75%] bg-black rounded-t-full border-white shadow-[0px_0px_4px_.4px_#00000036]" style={{borderWidth: '0.66vh', borderStyle: 'solid'}}>
                         <Image 
                             src={smurfCat}
                             alt="something"
-                            width={132}
-                            height={144}
+                            style={{ width: '100%', height: '100%' }}
                             className='rounded-t-full'
                         />
                     </div>
-                    <h1 className="text-2xl font-semibold text-center text-black">
+                    <h1 className="text-[2.63vh] font-semibold text-center text-black">
                         Jane Doe #1<br/>
                         Intania
                     </h1>
@@ -50,7 +49,7 @@ export default function Home() {
             
             
 
-            <center className="mt-5 mb-5">
+            <center className="my-[1.32vh]">
                 <Image 
                     src={something}
                     alt="something"
@@ -60,37 +59,35 @@ export default function Home() {
             </center>
             
             
-            <div className="w-full flex items-center flex-col gap-y-4 font-medium text-xl">
-                <div className="bg-[#FFBBD2] w-4/5 h-12 rounded-lg drop-shadow-md place-content-center">
+            <div className="w-full flex items-center flex-col h-[26.34vh] justify-between font-medium text-[2.2vh]">
+                <div className="bg-[#FFBBD2] w-4/5 h-[5.26vh] rounded-lg drop-shadow-md place-content-center">
                     <div className="flex space-x-1 justify-center">
                         <div>CU First Date 2024</div>
                         <div></div>
                     </div>
                 </div>
-                <div className="bg-[#E9A49B] w-4/5 h-12 rounded-lg drop-shadow-md place-content-center">
+                <div className="bg-[#E9A49B] w-4/5 h-[5.26vh] rounded-lg drop-shadow-md place-content-center">
                     <div className="flex space-x-1 justify-center">
                         <div>Rub Peun Kao Mai 2024</div>
                         <div></div>
                     </div>
                 </div>
-                <div className="bg-[#F1DFC1] w-4/5 h-12 rounded-lg drop-shadow-md place-content-center">
-                    <div className="flex space-x-1 justify-center">
+                <div className="bg-[#F1DFC1] w-4/5 h-[5.26vh] rounded-lg drop-shadow-md place-content-center">
+                    <div className="flex space-x-1 justify-center items-center">
                         <Image 
                             src={eBookIcon} 
                             alt="E-book Icon" 
-                            width={24} 
-                            height={24} 
+                            style={{ width: '2.63vh', height: '2.63vh' }} 
                         />
                         <div>E-Book</div>
                     </div>
                 </div>
-                <div className="bg-[#313131] w-4/5 h-12 rounded-lg drop-shadow-md place-content-center">
-                    <div className="flex space-x-1 justify-center text-white">
+                <div className="bg-[#313131] w-4/5 h-[5.26vh] rounded-lg drop-shadow-md place-content-center">
+                    <div className="flex space-x-1 justify-center items-center text-white">
                         <Image 
                             src={contactIcon} 
                             alt="Contact List Icon" 
-                            width={24} 
-                            height={24} 
+                            style={{ width: '2.63vh', height: '2.63vh' }} 
                         />
                         <div>Emergency Contact</div>
                     </div>
@@ -99,25 +96,23 @@ export default function Home() {
 
 
 
-            <div className="flex justify-center gap-x-4 mt-6">
-                <Link href="/" className="flex flex-col items-center text-center gap-y-1">
-                    <div className="w-11 h-11 rounded-full flex justify-center shadow-[0px_3px_4px_.5px_#00000048]">
+            <div className="flex justify-center gap-x-[1.76vh] mt-[1.32vh] text-[1.76vh]">
+                <Link href="/" className="flex flex-col items-center text-center gap-y-[0.44vh]">
+                    <div className="w-[4.8vh] h-[4.8vh] rounded-full flex justify-center items-center shadow-[0px_3px_4px_.5px_#00000048]">
                         <Image 
                                 src={qrcodeIcon} 
                                 alt="QR Code Icon" 
-                                width={24} 
-                                height={24} 
+                                style={{ width: '2.63vh', height: '2.63vh' }} 
                         />
                     </div>
                     <div>My Qr</div>
                 </Link>
-                <Link href="/" className="flex flex-col items-center text-center gap-y-1">
-                    <div className="w-11 h-11 rounded-full flex justify-center shadow-[0px_3px_4px_.5px_#00000048]">
+                <Link href="/" className="flex flex-col items-center text-center gap-y-[0.44vh]">
+                    <div className="w-[4.8vh] h-[4.8vh] rounded-full flex justify-center items-center shadow-[0px_3px_4px_.5px_#00000048]">
                         <Image 
                                 src={editIcon} 
                                 alt="Edit Icon" 
-                                width={24} 
-                                height={24} 
+                                style={{ width: '2.63vh', height: '2.63vh' }} 
                         />
                     </div>
                     <div>แก้ไขข้อมูล</div>
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,7 +2,7 @@ import Border from '@/components/Border';
 import FDLogo from '@public/FIrst Date Logo.svg';
 import MapImage from '@public/map.svg';
 import Image from 'next/image';
-import { Icon } from '@iconify/react';
+//import { Icon } from '@iconify/react';
 
 export default function Map() {
   return (
@@ -26,10 +26,10 @@ export default function Map() {
         </div>
       </Border>
       <div className="absolute top-6 right-4 flex flex-col items-center">
-        <Icon
+        {/* <Icon
           icon="icon-park-solid:people"
           className="w-11 h-11 p-3 rounded-full bg-white text-black"
-        />
+        /> */}
         <span className="font-athiti font-medium">โปรไฟล์</span>
       </div>
     </main>
