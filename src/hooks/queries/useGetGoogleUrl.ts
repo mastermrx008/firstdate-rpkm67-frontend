4a71271258be58ab80485804257e487ade6556a2
teegoood <teegooodgood@gmail.com>
2024-07-03 21:09:54 +0700

    refactor: add googleAuth folder

--- src/hooks/queries/useGetGoogleUrl.ts
+++ src/hooks/queries/useGetGoogleUrl.ts
@@ -1,4 +1,4 @@
-import { getGoogleUrl } from '@/api/getGoogleUrl';
+import { getGoogleUrl } from '@/api/googleAuth/getGoogleUrl';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetGoogleUrl = (isReady: boolean) => {
