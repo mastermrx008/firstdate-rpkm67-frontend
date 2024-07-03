7b58ecfc5815e2d8cc780b18168322fed48ec0e3
teegoood <teegooodgood@gmail.com>
2024-07-03 19:15:28 +0700

    refactor: change .env base_url value

--- src/api/googleLogin.ts
+++ src/api/googleLogin.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getGoogleUrl = async () => {
   const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/google-url`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/auth/google-url`
   );
   const data = res.data;
   const url = data.url;
