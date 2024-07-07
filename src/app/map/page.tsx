48b31aefcfd5b8702e7971f7c1accaf7f09e29ac
ThanaphomH <sqoulo11@gmail.com>
2024-07-08 02:17:53 +0700

    feat: back button at /map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,13 +1,19 @@
+"use client"
 import Border from '@/components/Border';
 import FDLogo from '@public/FIrst Date Logo.svg';
 import MapImage from '@public/map.svg';
 import Image from 'next/image';
 import { Icon } from '@iconify/react';
+import { useRouter } from 'next/navigation';
 
 export default function Map() {
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  }
   return (
     <main className="w-full h-screen flex justify-center items-center flex-col bg-2">
-      <Border variant="white">
+      <Border variant="white" className='gap-5 px-[10%]'>
         <div className="flex w-full justify-center">
           <Image
             src={FDLogo}
@@ -15,15 +21,22 @@ export default function Map() {
             className="w-44"
           />
         </div>
-        <div className="font-season italic text-2xl mt-6">Map</div>
+        <div className="font-season italic text-2xl">Map</div>
         <Image
           src={MapImage}
           alt="map"
-          className="w-full aspect-auto mt-5"
+          className="w-full aspect-[274/342] object-cover"
         />
-        <div className="font-season italic text-2xl text-center p-4 mt-5 bg-project-fuchsia">
+        <div className="font-season italic text-2xl text-center p-4 w-full bg-project-fuchsia">
           Lorem ipsum dolor sit
         </div>
+        <button
+          onClick={handleBack}
+          className="w-64 h-12 font-medium text-black text-xl rounded-lg border-black border flex justify-center items-center"
+        >
+          กลับ
+        </button>
+
       </Border>
       <div className="absolute top-6 right-4 flex flex-col items-center">
         <Icon
