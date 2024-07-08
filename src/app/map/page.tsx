3a444c327830e3d1bdafd3f1364d5b133c0c2ee0
ThanaphomH <sqoulo11@gmail.com>
2024-07-08 14:51:19 +0700

    refactor: remove icon and some styling

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -15,7 +15,7 @@ export default function Map() {
     <main className="w-full h-screen flex justify-center items-center flex-col bg-2">
       <Border
         variant="white"
-        className="gap-5 px-[10%]"
+        className="gap-4 !pb-[30%]"
       >
         <div className="flex w-full justify-center">
           <Image
@@ -25,28 +25,24 @@ export default function Map() {
           />
         </div>
         <div className="font-season italic text-2xl">Map</div>
+
         <Image
           src={MapImage}
           alt="map"
           className="w-full aspect-[274/342] object-cover"
         />
+
         <div className="font-season italic text-2xl text-center p-4 w-full bg-project-fuchsia">
           Lorem ipsum dolor sit
         </div>
+
         <button
           onClick={handleBack}
-          className="w-64 h-12 font-medium text-black text-xl rounded-lg border-black border flex justify-center items-center"
+          className="w-3/4 h-12 font-medium text-black text-xl rounded-lg border-black border flex justify-center items-center"
         >
           กลับ
         </button>
       </Border>
-      <div className="absolute top-6 right-4 flex flex-col items-center">
-        <Icon
-          icon="icon-park-solid:people"
-          className="w-11 h-11 p-3 rounded-full bg-white text-black"
-        />
-        <span className="font-athiti font-medium">โปรไฟล์</span>
-      </div>
     </main>
   );
 }
