8767c27cc142fce5a2cfd0be6435e10fe1e8987e
Soravit Satechaichan <dunk.soravit@gmail.com>
2024-07-08 17:39:22 +0700

    fix: button coloring when path is correct, white when path is not home or profile fix: /firstdate/...

--- src/components/TwoCircleMenu.tsx
+++ src/components/TwoCircleMenu.tsx
@@ -1,31 +1,53 @@
 'use client';
-
 import { useAuth } from '@/context/AuthContext';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function TwoCircleMenu() {
   const { user } = useAuth();
   const router = useRouter();
+  const [profileActive, setProfileActive] = useState(false);
+  const [homeActive, setHomeActive] = useState(false);
+  const [selectedPath, setSelectedPath] = useState<string>('');
+
+  const handleNavigate = (path: string) => {
+    router.push(`${user?.role === 'staff' ? '/staff' : ''}${path}`);
+    setSelectedPath(path);
+  };
+
+  const handleClick = (iconName: 'home' | 'profile') => {
+    setProfileActive(iconName === 'profile');
+    setHomeActive(iconName === 'home');
+  };
+
   return (
     <div>
       <div className="absolute top-6 right-4 flex flex-col items-center">
         <Icon
           icon="icon-park-solid:people"
-          className="w-11 h-11 p-3 rounded-full bg-white text-black"
-          onClick={() =>
-            router.push(`${user?.role === 'staff' ? '/staff' : ''}/profile`)
-          }
+          className={`w-11 h-11 p-3 rounded-full ${
+            profileActive
+              ? 'bg-project-fuchsia text-white'
+              : 'bg-white text-black'
+          }`}
+          onClick={() => {
+            handleClick('profile');
+            handleNavigate('/firstdate/profile');
+          }}
         />
         <span className="font-athiti font-medium">โปรไฟล์</span>
       </div>
       <div className="absolute top-6 left-4 flex flex-col items-center">
         <Icon
           icon="icon-park-solid:home"
-          className="w-11 h-11 p-3 rounded-full bg-white text-black"
-          onClick={() =>
-            router.push(`${user?.role === 'staff' ? '/staff' : ''}/home`)
-          }
+          className={`w-11 h-11 p-3 rounded-full ${
+            homeActive ? 'bg-project-fuchsia text-white' : 'bg-white text-black'
+          }`}
+          onClick={() => {
+            handleClick('home');
+            handleNavigate('/firstdate/home');
+          }}
         />
         <span className="font-athiti font-medium">หน้าหลัก</span>
       </div>
