1a44b66e820413020180e6af58507ac9174016f0
PhorDotC <por2547.c@gmail.com>
2024-07-11 03:30:34 +0700

    fix: responsive and menu

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,7 @@
 import Border from '@/components/Border';
 import FDLogo from '@public/FIrst Date Logo.svg';
 import bowLine from '@public/stat/bowline.svg';
-import TwoCircleMenu from '@/components/TwoCircleMenu';
+import MenuList from '@/components/MenuList';
 import UserInfo from '@/components/profile/UserInfo'
 import StatBars from "@/components/profile/StatBars"
 import Image from 'next/image';
@@ -15,34 +15,34 @@ export default function Profile(){
     if( !user ) return;
 
     return (
-        <main className="flex justify-center items-center flex-col bg-2">
-            <TwoCircleMenu />
-            <div className="flex justify-center items-center w-full h-screen overflow-y-auto">
+        <>
+            <div className="relative flex justify-center items-center w-full">
             <Border variant="light-pink">
-                <div className="flex flex-col items-center z-10">
+                <div className="absolute top-[7%] w-[70%] flex flex-col items-center z-10">
                     <Image
                         src={FDLogo}
                         alt="logo"
-                        className="w-44 mt-5 mb-2"
+                        className="w-[60%] mb-2"
                     />
                     <UserInfo user={user}/>
                     <Image
                         src={bowLine}
                         alt="bowLine"
-                        className="w-32 mb-3 mt-1"
+                        className="w-[45%] mb-3 mt-1"
                     />
-                    <StatBars stamp={user?.stamp}/>
-                    <div className="w-5/6 mt-10 z-10 drop-shadow">
+                    <StatBars stamp={user?.stamp}/>                         
+                </div>
+                <div className="absolute bottom-[5%] w-[65%] h-[6%] z-10 drop-shadow">
                         <Link
                             href="/firstdate/home"
-                            className="flex flex-row items-center justify-center bg-project-pink px-2 py-[10px] gap-x-2 rounded-lg text-white"
+                            className="flex flex-row items-center justify-center bg-project-pink px-2 py-[10px] h-[100%] gap-x-2 rounded-lg text-white"
                         >
                             <p className="font-athiti text-xl font-medium text-black">กลับไปเดินทางต่อ</p>
                         </Link>
-                    </div>
                 </div>               
             </Border>
-            </div>       
-        </main>
+            <MenuList />
+            </div>
+        </>       
     )
 }
--- src/components/profile/userInfo.tsx
+++ src/components/profile/userInfo.tsx
@@ -10,48 +10,50 @@ interface Info {
 
 export default function UserInfo({ user }: Info) {
   return (
-    <div className="flex justify-center gap-2 items-end">
-      <div className="relative flex justify-center items-center w-[66px]">
-        <Link
-            className="absolute bottom-9 flex items-center flex-col"
-            href="/edit"
-        >
-            <Icon
-            icon="ri:edit-fill"
-            className="size-11 p-2.5 rounded-full bg-white text-black drop-shadow"
-            />
-            <span className="text-center font-medium text-base">แก้ไขข้อมูล</span>
-        </Link>
-      </div>
-      <div className="flex flex-col justify-center text-center">
-        <div className="flex justify-center items-center w-36 h-48 mb-2 bg-white rounded-t-full drop-shadow">
-          <Image
-            src={user?.photoUrl || placeholder}
-            alt="profile"
-            className={`w-full object-cover rounded-t-full p-1.5
-                            ${user?.photoUrl ? ' h-48' : 'mt-5 h-32'}
-                            `}
-          />
+    <div className="w-auto h-full">
+        <div className="flex justify-center gap-2">
+        <div className="flex justify-center items-center w-[66px]">
+            <Link
+                className="mt-8 flex items-center flex-col"
+                href="/edit"
+            >
+                <Icon
+                icon="ri:edit-fill"
+                className="size-11 p-2.5 rounded-full bg-white text-black drop-shadow"
+                />
+                <span className="text-center font-medium text-base">แก้ไขข้อมูล</span>
+            </Link>
         </div>
-        <div className="flex justify-center items-center flex-wrap font-semibold text-2xl gap-2 max-w-36">
-          <span>{user?.firstname}</span>
-          <span>{user?.lastname}</span>
-          <span>#{user?.year}</span>
-        </div>
-        <span className="font-semibold text-2xl">{user?.faculty}</span>
-      </div>
-      <div className="relative flex justify-center items-center w-[66px]">
-        <Link
-            className="absolute bottom-9 flex items-center flex-col"
-            href="/firstdate/map"
-        >
-            <Icon
-            icon="material-symbols:map"
-            className="size-11 p-2.5 rounded-full bg-white text-black drop-shadow"
+        <div className="flex flex-col justify-center items-center text-center">
+            <div className="flex justify-center items-center w-[16vh] h-[22vh] mb-2 bg-white rounded-t-full drop-shadow">
+            <Image
+                src={user?.photoUrl || placeholder}
+                alt="profile"
+                className={`w-full rounded-t-full p-1.5
+                                ${user?.photoUrl ? 'h-[22vh] object-cover' : 'mt-5 h-[14vh]'}
+                                `}
             />
-            <span className="text-center font-medium text-base">แผนที่</span>
-        </Link>
-      </div>
+            </div>
+            <div className="flex justify-center items-center font-semibold text-xl gap-2 max-w-36">
+            <span>{user?.firstname}</span>
+            <span>{user?.lastname}</span>
+            <span>#{user?.year}</span>
+            </div>
+            <span className="font-semibold text-xl">{user?.faculty}</span>
+        </div>
+        <div className="flex justify-center items-center w-[66px]">
+            <Link
+                className="mt-8 flex items-center flex-col"
+                href="/firstdate/map"
+            >
+                <Icon
+                icon="material-symbols:map"
+                className="size-11 p-2.5 rounded-full bg-white text-black drop-shadow"
+                />
+                <span className="text-center font-medium text-base">แผนที่</span>
+            </Link>
+        </div>
+        </div>
     </div>
   );
 }
