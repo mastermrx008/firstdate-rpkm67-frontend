42ceead89bc5003a6a5850d3fb8d6febe0ceb476
teegoood <teegooodgood@gmail.com>
2024-07-10 16:16:02 +0700

    refactor: rewarddone

--- src/app/rewarddone/page.tsx
+++ src/app/rewarddone/page.tsx
@@ -12,7 +12,7 @@ export default function RewardDone() {
   const [isOpenEndingModal, setIsOpenEndingModal] = useState<boolean>(false);
 
   return (
-    <main className="w-full h-screen flex justify-center items-center flex-col bg-2">
+    <>
       <Border
         variant="white-brown"
         className="flex flex-col"
@@ -20,7 +20,7 @@ export default function RewardDone() {
         <Image
           src={FDLogo}
           alt="logo"
-          className="w-44 mb-20"
+          className="w-[60%] my-[20%]"
         />
         <h1 className="text-3xl text-center font-semibold text-project-light-gray mb-1">
           แลกรับของรางวัล
@@ -34,13 +34,13 @@ export default function RewardDone() {
         />
         <button
           onClick={() => setIsOpenEndingModal(true)}
-          className="mt-3 w-64 h-12 font-medium text-white text-xl rounded-lg bg-project-fuchsia flex justify-center items-center"
+          className="mt-4 w-[70%] py-2 font-medium text-white text-xl rounded-lg bg-project-fuchsia flex justify-center items-center"
         >
           สิ้นสุดการเดินทาง
         </button>
         <Link
           href={'/firstdate/home'}
-          className="mt-4 w-64 h-12 font-medium text-black text-xl rounded-lg border-black border flex justify-center items-center"
+          className="mt-4 w-[70%] py-2 font-medium text-black text-xl rounded-lg border-black border flex justify-center items-center"
         >
           กลับไปเดินทางต่อ
         </Link>
@@ -50,6 +50,6 @@ export default function RewardDone() {
         isOpen={isOpenEndingModal}
         onOpenChange={setIsOpenEndingModal}
       />
-    </main>
+    </>
   );
 }
