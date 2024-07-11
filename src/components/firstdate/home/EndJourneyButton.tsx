2204885fe4b3ce76dfddac404e39b3b1fa1ad7f9
teegoood <teegooodgood@gmail.com>
2024-07-11 21:07:48 +0700

    add: ending modal to firstdate home page

--- src/components/firstdate/home/EndJourneyButton.tsx
+++ src/components/firstdate/home/EndJourneyButton.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const EndJourneyButton = () => {
+interface EndJourneyButton {
+  handleOnClick: () => void;
+}
+
+const EndJourneyButton = ({ handleOnClick }: EndJourneyButton) => {
   return (
-    <div className="flex items-center justify-center px-16 py-2 bg-project-fuchsia rounded-xl text-white text-center text-nowrap">
+    <div
+      className="flex items-center justify-center px-16 py-2 bg-project-fuchsia rounded-xl text-white text-center text-nowrap cursor-pointer"
+      onClick={handleOnClick}
+    >
       สิ้นสุดการเดินทาง
     </div>
   );
