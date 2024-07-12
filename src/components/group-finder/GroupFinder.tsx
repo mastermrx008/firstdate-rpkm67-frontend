5c2653b97dfd5d2dc6fb2d2c9015696d7d5eae54
ThanaphomH <sqoulo11@gmail.com>
2024-07-12 21:14:08 +0700

    feat: basic groupfinder component

---
+++ src/components/group-finder/GroupFinder.tsx
@@ -0,0 +1,14 @@
+const GroupFinder = () => {
+    return ( 
+        <div className="flex flex-col bg-project-light-gray bg-opacity-90 p-4 w-full items-center gap-4">
+            <span className="font-athiti font-bold text-xl text-project-cream">จับคู่เพื่อน [0/2]</span>
+            <span className="font-athiti font-semibold text-3xl text-white">คุณยังไม่ได้จับคู่</span>
+            <span className="font-athiti font-medium text-project-cream">*สามารถเลือกบ้านคนเดียวได้</span>
+            <div>
+
+            </div>
+        </div>
+     );
+}
+ 
+export default GroupFinder;
