c1982f8837dbe3ee8218df55d446ec1b637a4dc0
teegoood <teegooodgood@gmail.com>
2024-07-14 01:57:35 +0700

    add: image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.BASE_DOMAIN_IMAGES,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
