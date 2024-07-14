fd7f07e58b7ef5e7ea0af0607c50337819400565
Idhibhat Pankam <marginpankam@gmail.com>
2024-07-14 15:10:36 +0700

    fix: load env from server

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.API_BASE_URL,
+    NEXT_PUBLIC_FIRST_DATE_DATE: process.env.FIRST_DATE_DATE,
+    NEXT_PUBLIC_RUP_PEUN_DATE: process.env.RUP_PEUN_DATE,
+  },
 };
 
 export default nextConfig;
