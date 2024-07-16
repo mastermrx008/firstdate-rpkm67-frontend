fd00fc8a4db62d09b0797b702d911baa918f4cf6
teegoood <teegooodgood@gmail.com>
2024-07-16 21:17:58 +0700

    fix: finish-register and otp modal style

--- src/components/firstdate/home/modal/OTPModal.tsx
+++ src/components/firstdate/home/modal/OTPModal.tsx
@@ -87,7 +87,7 @@ const OTPModal: React.FC<OTPModalProps> = ({
                 กรุณากรอก OTP เพื่อทำการยืนยันกิจกรรม
               </h3>
               {reminderText && (
-                <p className="font-normal">สถานที่รับ: {reminderText}</p>
+                <p className="font-normal text-center">สถานที่รับ: {reminderText}</p>
               )}
             </div>
           </div>
