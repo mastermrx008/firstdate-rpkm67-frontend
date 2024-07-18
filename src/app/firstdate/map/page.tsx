34b23fe2200313469a549bbe1bf908c591f546b7
Thanaphom Hirunyathon <thanaphom.h@gmail.com>
2024-07-18 15:39:06 +0700

    refactor: lint:fix

--- src/app/firstdate/map/page.tsx
+++ src/app/firstdate/map/page.tsx
@@ -3,7 +3,7 @@ import FDLogo from '@public/FIrst Date Logo.svg';
 import MapImage from '@public/mapPR.png';
 import Image from 'next/image';
 import Border from '@/components/firstdate/Border';
-import Zoom from "next-image-zoom";
+import Zoom from 'next-image-zoom';
 import Link from 'next/link';
 
 export default function Map() {
@@ -12,7 +12,7 @@ export default function Map() {
       variant="white"
       className="gap-4 pt-[5%] pb-[30%] px-[8%] justify-between"
     >
-      <div className='flex flex-col gap-4'>
+      <div className="flex flex-col gap-4">
         <div className="flex w-full justify-center">
           <Image
             src={FDLogo}
@@ -23,9 +23,12 @@ export default function Map() {
         <div className="font-season italic text-2xl text-center">Map</div>
       </div>
 
-      <div className='flex w-full items-center'>
-        <Zoom src={MapImage} alt='map' className='object-cover w-full aspect-[1524/1080]'>
-        </Zoom>
+      <div className="flex w-full items-center">
+        <Zoom
+          src={MapImage}
+          alt="map"
+          className="object-cover w-full aspect-[1524/1080]"
+        ></Zoom>
       </div>
 
       <Link
