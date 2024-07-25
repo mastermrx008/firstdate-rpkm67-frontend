edb414e9152f97cd1b8d8d763597343792b4171b
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-07-25 23:04:26 +0700

    fix: page padding

--- src/app/rpkm/freshy-night/register-done/page.tsx
+++ src/app/rpkm/freshy-night/register-done/page.tsx
@@ -14,9 +14,9 @@ export default function RegisterDone() {
 
   return (
     <>
-      <div className="bg-[#FFFEF7E5] w-11/12 mx-auto py-[10%] flex flex-col items-center gap-y-5 [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]">
+      <div className="bg-[#FFFEF7E5] w-11/12 mx-auto py-[5%] flex flex-col items-center gap-y-5 [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]">
         <div className="text-center font-sopha font-outline">
-          <p className="text-project-pastel-pink text-8xl/[5rem] -mt-7 [text-shadow:_2px_2px_2px_rgb(0_0_0_/_30%)]">
+          <p className="text-project-pastel-pink text-8xl/[5rem] [text-shadow:_2px_2px_2px_rgb(0_0_0_/_30%)]">
             ลงทะเบียน
           </p>
           <p className="text-project-yellow text-9xl/[2.5rem] tracking-wide [text-shadow:_2px_2px_2px_rgb(0_0_0_/_30%)]">
