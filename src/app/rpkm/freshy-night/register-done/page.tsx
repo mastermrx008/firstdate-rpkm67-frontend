ee8f78200a67fe9578c3ac216991cb8105806353
teegoood <teegooodgood@gmail.com>
2024-07-28 21:32:02 +0700

    fix: remove wait text

--- src/app/rpkm/freshy-night/register-done/page.tsx
+++ src/app/rpkm/freshy-night/register-done/page.tsx
@@ -14,7 +14,7 @@ export default function RegisterDone() {
 
   return (
     <div className="py-[8%]">
-      <div className="bg-[#FFFEF7E5] w-11/12 mx-auto py-[5%] flex flex-col items-center gap-y-5 [box-shadow:_0px_0px_15px_0px_rgba(0_0_0_0.50)] [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]">
+      <div className="bg-[#FFFEF7E5] w-11/12 justify-between mx-auto py-[5%] flex flex-col items-center gap-y-5 min-h-screen [box-shadow:_0px_0px_15px_0px_rgba(0_0_0_0.50)] [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]">
         <div className="text-center font-sopha font-outline">
           <p className="text-project-pastel-pink text-8xl/[5rem] [text-shadow:_2px_2px_2px_rgb(0_0_0_/_30%)]">
             ลงทะเบียน
@@ -22,9 +22,6 @@ export default function RegisterDone() {
           <p className="text-project-yellow text-9xl/[2.5rem] tracking-wide [text-shadow:_2px_2px_2px_rgb(0_0_0_/_30%)]">
             <span className="-mr-5">ส</span> ำเร็จ
           </p>
-        </div>
-        <div className="w-full h-[50vh] flex justify-center items-center">
-          <p className="text-3xl font-mono font-bold">Waited...</p>
         </div>
         <BottomButton
           onClick={() => setQrModal(true)}
