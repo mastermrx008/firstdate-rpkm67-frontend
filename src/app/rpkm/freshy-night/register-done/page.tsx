0092d913ab2a34cf39d8f0422d5187cf9c7651d1
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-07-31 13:54:49 +0700

    feat: freshy night poster

--- src/app/rpkm/freshy-night/register-done/page.tsx
+++ src/app/rpkm/freshy-night/register-done/page.tsx
@@ -2,10 +2,12 @@
 
 import { useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
+import Image from 'next/image';
 
 import BottomButton from '@/components/(main)/home/BottomButton';
 import QrCodeModal from '@/components/rpkm/freshy-night/QrCodeModal';
 import qrcodeIcon from '@public/home/icon/qrcode.svg';
+import poster from '@public/rpkm/freshy-night/poster.jpg';
 
 export default function RegisterDone() {
   const [qrModal, setQrModal] = useState<boolean>(false);
@@ -14,7 +16,7 @@ export default function RegisterDone() {
 
   return (
     <div className="py-[8%]">
-      <div className="bg-[#FFFEF7E5] w-11/12 justify-between mx-auto py-[5%] flex flex-col items-center gap-y-5 min-h-screen [box-shadow:_0px_0px_15px_0px_rgba(0_0_0_0.50)] [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]">
+      <div className="bg-[#FFFEF7E5] w-11/12 mx-auto py-[5%] flex flex-col items-center gap-y-[3em]  [box-shadow:_0px_0px_15px_0px_rgba(0_0_0_0.50)] [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]">
         <div className="text-center font-sopha font-outline">
           <p className="text-project-pastel-pink text-8xl/[5rem] [text-shadow:_2px_2px_2px_rgb(0_0_0_/_30%)]">
             ลงทะเบียน
@@ -23,6 +25,11 @@ export default function RegisterDone() {
             <span className="-mr-5">ส</span> ำเร็จ
           </p>
         </div>
+        <Image
+          src={poster}
+          alt="Freshy night poster"
+          className="w-[85%]"
+        />
         <BottomButton
           onClick={() => setQrModal(true)}
           src={qrcodeIcon}
