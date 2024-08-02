3acec415d3c309fbe4bf86430a35f1e9fcdc59a3
teegoood <teegooodgood@gmail.com>
2024-08-02 17:01:10 +0700

    fix: remove border bg-repeat

--- src/components/rpkm/freshy-night/Base.tsx
+++ src/components/rpkm/freshy-night/Base.tsx
@@ -18,12 +18,9 @@ export default function Base(props: BaseProps) {
     <>
       <div
         className={cn(
-          'flex flex-col items-center w-[85%] min-h-[calc(70vw*(801/371))] my-[5%] p-[10%] mx-auto [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]',
+          'bg-[#FFFEF7E5] flex flex-col items-center w-[85%] min-h-[calc(70vw*(801/371))] my-[5%] p-[10%] mx-auto [clip-path:polygon(1rem_0,calc(100%-1rem)_0,100%_1rem,100%_calc(100%-1rem),calc(100%-1rem)_100%,1rem_100%,0_calc(100%-1rem),0_1rem)]',
           className
         )}
-        style={{
-          backgroundImage: `url(${border.src})`,
-        }}
       >
         {children}
       </div>
